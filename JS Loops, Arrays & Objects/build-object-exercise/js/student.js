fdf5ebdfb5b64eeac8ea37e9b8416ac0ba5401ca
Steph CP <stephers.cp@gmail.com>
2018-07-31 17:35:41 -0400

    “Build an object” exercise in “JS Loops, Arrays & Objects” course

---
+++ JS Loops, Arrays & Objects/build-object-exercise/js/student.js
@@ -0,0 +1,26 @@
+var students = [
+  {
+    name: 'Dave',
+    track: 'PHP Development',
+    achievements: 158,
+    points: 14730
+  },
+  {
+  name: 'Jody',
+  track: 'VR with Unity and C#',
+  achievements: '175',
+  points: '16375'
+  },
+  {
+  name: 'Stephanie',
+  track: 'Front End Development',
+  achievements: '5',
+  points: '1583'
+  },
+  {
+  name: 'Jon',
+  track: 'Back End Development',
+  achievements: '15',
+  points: '2483'
+  },
+]
