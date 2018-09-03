b71a4997e3e867d124ab3437744014f90720159e
Stephanie CP <stephers.cp@gmail.com>
2018-09-03 14:55:34 -0700

    first commit
    frame is up. React needs to be added. JS needs to be added for functionality of keys

---
+++ React/calculator/index.js
@@ -0,0 +1,17 @@
+var *** = React.createClass({
+    render: function(){
+        return (
+            // JSX
+        );
+    }
+});
+
+var App = React.createClass({
+    render: function(){
+        return (
+
+        );
+    }
+});
+
+React.render(<App />,  document.getElementById("content"));
---
+++ React/calculator/like_button.js
@@ -0,0 +1,23 @@
+// "started code" pasted below
+'use strict';
+const e = React.createElement;
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+        return e(
+            'button',
+            { onClick: () => this.setState({ liked: true }) },
+            'Like'
+        );
+    }
+}
+
+// two line to add after the "starter code"
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
